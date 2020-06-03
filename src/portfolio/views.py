3ab01f3ead0d61ff1604c81dfaac6fc7946d8892
edward-kolodziejski <edward.kolodziejski@pm.me>
2020-06-03 15:11:47 +0200

    Form has been created, template has been updated

--- src/portfolio/views.py
+++ src/portfolio/views.py
@@ -4,18 +4,24 @@ from django.shortcuts import render
 
 def home_page(request):
     context = {
-        "title":"Hello, World!"
+        "title":"Hello, World!",
+        "content":"Welcome to the homepage."
         }
     return render(request, "home_page.html", context)
 
 def about_page(request):
     context = {
-        "title":"About Page"
+        "title":"About Page",
+        "content":"Welcome to the about page."
     }
     return render(request, "home_page.html", context)
     
 def contact_page(request):
     context = {
-        "title":"Contact"
+        "title":"Contact",
+        "content":"Welcome to the contact page."
     }
-    return render(request, "home_page.html", context)
+    if request.method == "POST":
+        print(request.POST)
+        print(request.POST.get("fullname"))
+    return render(request, "contact/view.html", context)
