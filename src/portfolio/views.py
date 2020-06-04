b7200e7c45fc54cfb43a282e7711b1230da86534
edward-kolodziejski <edward.kolodziejski@pm.me>
2020-06-04 13:05:40 +0200

    contact form has been updated

--- src/portfolio/views.py
+++ src/portfolio/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .forms import ContactForm
 
 def home_page(request):
     context = {
@@ -17,11 +18,12 @@ def about_page(request):
     return render(request, "home_page.html", context)
     
 def contact_page(request):
+    contact_form = ContactForm(request.POST or None)
     context = {
         "title":"Contact",
-        "content":"Welcome to the contact page."
+        "content":"Welcome to the contact page.",
+        "form":contact_form
     }
-    if request.method == "POST":
-        print(request.POST)
-        print(request.POST.get("fullname"))
+    if contact_form.is_valid():
+        print(contact_form.cleaned_data)
     return render(request, "contact/view.html", context)
