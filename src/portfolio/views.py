1cd2d63e331765bb17436bcb1992d4b97cb22eee
edward-kolodziejski <edward.kolodziejski@pm.me>
2020-06-04 21:11:30 +0200

    login form update, register form has been created

--- src/portfolio/views.py
+++ src/portfolio/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth import authenticate, login, get_user_model
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
-from .forms import ContactForm, LoginForm
+from .forms import ContactForm, LoginForm, RegisterForm
 
 
 def login_page(request):
@@ -22,17 +22,27 @@ def login_page(request):
         if user is not None:
             # print(request.user.is_authenticated())
             login(request, user)
-            return redirect("/login")
+            return redirect("/")
         else:
             print("Error")
 
     return render(request, "auth/login.html", context)
 
+
+User = get_user_model()
 def register_page(request):
     form = RegisterForm(request.POST or None)
+    context = {
+        "form": form
+    }
     if form.is_valid():
         print(form.cleaned_data)
-    return render(request, "auth/register.html", {})
+        username  = form.cleaned_data.get("username")
+        email  = form.cleaned_data.get("email")
+        password  = form.cleaned_data.get("password")
+        User.objects.create_user(username, email, password) 
+        print(new_user)
+    return render(request, "auth/register.html", context)
 
 def home_page(request):
     context = {
