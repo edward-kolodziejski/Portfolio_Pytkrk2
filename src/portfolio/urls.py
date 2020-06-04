b9da60c6d8fb324a03f95d04b66c8157c52e2eaa
edward-kolodziejski <edward.kolodziejski@pm.me>
2020-06-05 01:25:16 +0200

    product app update

--- src/portfolio/urls.py
+++ src/portfolio/urls.py
@@ -18,6 +18,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.conf.urls import url, include
 
+from products.views import ProductListView, product_list_view
 from .views import home_page, about_page, contact_page, login_page, register_page
 
 urlpatterns = [
@@ -26,6 +27,8 @@ urlpatterns = [
     url(r'^contact/$', contact_page),
     url(r'^login/$', login_page),
     url(r'^register/$', register_page),
+    url(r'^products/$', ProductListView.as_view()),
+    url(r'^products-fbv/$', product_list_view),
     url(r'^admin/', admin.site.urls),
     ]
 
--- src/products/views.py
+++ src/products/views.py
@@ -1,3 +1,15 @@
+from django.views.generic import ListView
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Product
+
+class ProductListView(ListView):
+    queryset = Product.objects.all()
+    template_name = "product/product_list_.html"
+
+def product_list_view(request):
+    queryset = Product.objects.all()
+    context = {
+        'object_list': queryset
+    }
+    return render(request, "product/product_list_.html", context)
