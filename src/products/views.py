6518355a0528f1430ceaa830444613932dd2e3fb
edward-kolodziejski <edward.kolodziejski@pm.me>
2020-06-10 21:09:39 +0200

    update on products

--- src/products/views.py
+++ src/products/views.py
@@ -1,15 +1,35 @@
-from django.views.generic import ListView
-from django.shortcuts import render
+from django.http import Http404
+from django.views.generic import ListView, DetailView
+from django.shortcuts import render, get_object_or_404
 
 from .models import Product
 
 class ProductListView(ListView):
     queryset = Product.objects.all()
-    template_name = "product/product_list_.html"
-
+    template_name = "products/list.html"
+    
 def product_list_view(request):
     queryset = Product.objects.all()
     context = {
         'object_list': queryset
     }
-    return render(request, "product/product_list_.html", context)
+    return render(request, "products/list.html", context)
+
+class ProductDetailView(ListView):
+    queryset = Product.objects.all()
+    template_name = "products/detail.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
+        return context
+
+def product_detail_view(request, pk=None, *args, **kwargs):
+    qs = Product.objects.filter(id=pk)
+    if qs.exists() and qs.count() == 1:
+        instance = qs.first()
+    else:
+        raise Http404("Product doesn't exist")
+    context = {
+        'object': instance
+    }
+    return render(request, "products/detail.html", context)
