164e2f4a7f3e5db1be19781f7b638a0cfcba5f50
edward-kolodziejski <edward.kolodziejski@pm.me>
2020-06-14 22:14:33 +0200

    bootstrap and navbar

--- src/portfolio/urls.py
+++ src/portfolio/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.conf.urls import url, include
+from django.views.generic import TemplateView
 
 from .views import home_page, about_page, contact_page, login_page, register_page
 
@@ -12,6 +13,7 @@ urlpatterns = [
     url(r'^login/$', login_page, name='login'),
     url(r'^register/$', register_page, name='register'),
     url(r'^products/', include("products.urls", namespace='products')),
+    url(r'^bootstrap/$', TemplateView.as_view(template_name='bootstrap/example.html')),
     url(r'^admin/', admin.site.urls),
     ]
 
