cc08d1edcb0309b48a51552dd64973050baeb662
edward-kolodziejski <edward.kolodziejski@pm.me>
2020-06-17 11:17:51 +0200

    search module added

--- src/portfolio/urls.py
+++ src/portfolio/urls.py
@@ -13,8 +13,9 @@ urlpatterns = [
     url(r'^login/$', login_page, name='login'),
     url(r'^register/$', register_page, name='register'),
     url(r'^products/', include("products.urls", namespace='products')),
+    url(r'^search/', include("search.urls", namespace='search')),
     url(r'^bootstrap/$', TemplateView.as_view(template_name='bootstrap/example.html')),
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls)
     ]
 
 if settings.DEBUG:
