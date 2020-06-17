2f8bf070c0e6bf2c63a2db251d94d51196e117cb
edward-kolodziejski <edward.kolodziejski@pm.me>
2020-06-17 20:36:32 +0200

    cart component initialized

--- src/portfolio/urls.py
+++ src/portfolio/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.conf.urls import url, include
 from django.views.generic import TemplateView
-
+from carts.views import cart_home
 from .views import home_page, about_page, contact_page, login_page, register_page
 
 urlpatterns = [
@@ -11,6 +11,7 @@ urlpatterns = [
     url(r'^about/$', about_page, name='about'),
     url(r'^contact/$', contact_page, name='contact'),
     url(r'^login/$', login_page, name='login'),
+    url(r'^cart/$', cart_home, name='cart'),
     url(r'^register/$', register_page, name='register'),
     url(r'^products/', include("products.urls", namespace='products')),
     url(r'^search/', include("search.urls", namespace='search')),
