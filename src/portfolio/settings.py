74e782ddc2469e6ce17acf9fe0ad872769eb987c
edward-kolodziejski <edward.kolodziejski@pm.me>
2020-06-19 17:54:24 +0200

    order component added

--- src/portfolio/settings.py
+++ src/portfolio/settings.py
@@ -38,9 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'carts',
+    'orders',
     'products',
     'search',
     'tags'
+
 ]
 
 MIDDLEWARE = [
